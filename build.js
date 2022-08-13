6b4aeb600f323ff65363dcf7315a4e2fbadf1dbf
Rxann <80224521+Rxann@users.noreply.github.com>
2022-08-13 08:31:55 -0400

    add plugin to build.js

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const esbuild = require("esbuild");
+const { typecheckPlugin } = require("@jgoz/esbuild-plugin-typecheck");
 const config = {
   target: "node16",
   sourcemap: true,
@@ -6,6 +7,7 @@ const config = {
   minify: true,
   entryPoints: ["./index.ts"],
   outdir: "./dist",
+  plugins: [typecheckPlugin()],
 };
 esbuild.build(config).catch((err) => {
   console.error(err);
