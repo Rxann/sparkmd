7c361116e5a823eb1941efaf4557ae957cd2801c
Rxann <80224521+Rxann@users.noreply.github.com>
2022-08-13 08:30:00 -0400

    Start basic parsing

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
 import * as fs from "fs";
+import * as colors from "nanocolors";
+import terminalLink from "terminal-link";
 
-export default function spark() {}
+export default function spark(file: fs.PathOrFileDescriptor) {
+  const data = fs.readFileSync(file, "utf-8").split("\n");
+  data.map((line) => {
+    if (line.startsWith("#")) {
+      console.log(colors.bold(line));
+    }
+    if (line.startsWith("##")) {
+      console.log(colors.bgBlack(line));
+    }
+    if (line.startsWith("###")) {
+      console.log(colors.underline(line));
+    }
+  });
+}
