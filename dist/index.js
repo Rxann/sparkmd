cadafc9b3fb3c4168cf41ee8811c84caad8c13cf
Rxann <80224521+Rxann@users.noreply.github.com>
2022-08-13 09:40:48 -0400

    new test build

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
-"use strict";export default function o(){}
+"use strict";import*as r from"fs";import*as t from"nanocolors";import a from"terminal-link";export default function i(s){r.readFileSync(s,"utf-8").split(`
+`).map(o=>{o.startsWith("#")&&console.log(t.bold(o)),o.startsWith("##")&&console.log(t.bgBlack(o)),o.startsWith("###")&&console.log(t.underline(o)),o.includes("[")&&o.includes("]")&&console.log(a(o,o.split("(")[1].split(")")[0]))})}
 //# sourceMappingURL=index.js.map
